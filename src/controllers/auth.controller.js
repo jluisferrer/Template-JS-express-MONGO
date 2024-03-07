6a0162608740d38d0da61860c4c46ddab96711da
jluisferrer <jluisferrer@gmail.com>
2024-03-07 12:35:56 +0100

    feat:user login

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 import bcrypt from "bcrypt"
 import User from "../models/User.js"
+import jwt  from "jsonwebtoken"
 
 
 export const register = async (req, res) => {
@@ -49,11 +50,69 @@ export const register = async (req, res) => {
     }
 }
 
-export const login = (req, res) => {
-    res.status(200).json(
-        {
-            sucess: true,
-            message: "Login user successfully"
-        }
-    )
+export const login = async (req, res) => {
+	try {
+		const email = req.body.email;
+		const password = req.body.password;
+
+		if (!email || !password) {
+			return res.status(400).json({
+				success: false,
+				message: "email and password are mandatories",
+			});
+		}
+
+		const validEmail = /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+		if (!validEmail.test(email)) {
+			return res.status(400).json({
+				success: false,
+				message: "Email format is not valid",
+			});
+		}
+
+		const user = await User.findOne({
+			email: email,
+		});
+
+		console.log(user);
+
+		if (!user) {
+			res.status(400).json({
+				success: false,
+				message: "Email or password invalid",
+			});
+		}
+
+		const isValidPassword = bcrypt.compareSync(password, user.password);
+
+		if (!isValidPassword) {
+			return res.status(400).json({
+				success: false,
+				message: "Email or password invalid",
+			});
+		}
+
+		const token = jwt.sign(
+			{
+				userId: user._id,
+				roleName: user.role,
+			},
+			process.env.JWT_SECRET,
+			{
+				expiresIn: "2h",
+			}
+		);
+
+		res.status(200).json({
+			success: true,
+			message: "User logged succesfully",
+			token: token, //MOSTRAMOS EL TOKEN DE MANERA TEMPORAL PARA PODER PROBAR CON ÉL OTRA FUNCIONALIDADES
+		});
+	} catch (error) {
+		res.status(500).json({
+			success: false,
+			message: "User cant be logged",
+			error: error,
+		});
+	}
 }
